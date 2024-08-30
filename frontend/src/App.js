836890e1bca6fbb134235eab84377d8b74de70fe
Preyanshu <mishrapreyanshu@gmail.com>
2024-08-30 22:39:15 +0530

    frontend + smart contract fixes
    Signed-off-by: Preyanshu <mishrapreyanshu@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import Main from './main';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+require('@solana/wallet-adapter-react-ui/styles.css');
+ 
+export default function App() {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+ 
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+ 
+    const wallets = useMemo(
+        () => [
+            /**
+             * Wallets that implement either of these standards will be available automatically.
+             *
+             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+             *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+             *   - Solana Wallet Standard
+             *     (https://github.com/anza-xyz/wallet-standard)
+             *
+             * If you wish to support a wallet that supports neither of those standards,
+             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+             * in the npm package `@solana/wallet-adapter-wallets`.
+             */
+            new UnsafeBurnerWalletAdapter(),
+        ],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [network]
+    );
+ 
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                    { /* Your app's components go here, nested within the context providers. */ }
+                    <Main />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("ATUuDhjiqaMYUMoky8ozqx3pDpZM1TDncnvd3tfRqAa2");
+declare_id!("5RmuPkbC8RZbYp3CF4FbV4xKT88MPjrDsngu2j7ZtVvR");
 
 #[program]
 pub mod crowdfunding {
@@ -18,21 +18,33 @@ pub mod crowdfunding {
         
     }
 
-    pub fn withdraw(ctx:Context<Withdraw>,amount:u64) -> ProgramResult {
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
         let campaign = &mut ctx.accounts.campaign;
         let user = &ctx.accounts.user;
+    
+        // Check if the user is the admin of the campaign
         if campaign.admin != *user.key {
             return Err(ProgramError::IncorrectProgramId);
         }
-        let rent_balance= Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
-
+    
+        // Calculate the minimum balance required to keep the account rent-exempt
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+    
+        // Ensure there are enough lamports in the campaign account to withdraw the desired amount
         if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
             return Err(ProgramError::InsufficientFunds);
         }
+    
+        // Perform the withdrawal by transferring lamports from the campaign to the user
         **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
-        **user.to_account_info().try_borrow_mut_lamports()?  += amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
+    
+        // Update the campaign's donated amount to reflect the withdrawal
+        campaign.amount_donated = campaign.amount_donated.saturating_sub(amount);
+    
         Ok(())
     }
+    
 
     pub fn donate(ctx:Context<Donate>,amount:u64) -> ProgramResult {
         let ix = anchor_lang::solana_program::system_instruction::transfer(
@@ -52,7 +64,7 @@ pub mod crowdfunding {
 
 #[derive(Accounts)]
 pub struct Create<'info> {
-    #[account(init, payer = user, space = 8 + 8 + 8 + 32 + 32,seeds =[ b"CAMPAIGN_DEMO".as_ref(),user.key().as_ref()],bump)]
+    #[account(init, payer = user, space = 10000,seeds =[ b"CAMPAIGN_DEMO".as_ref(),user.key().as_ref()],bump)]
     pub campaign: Account<'info, Campaign>,
     #[account(mut)]
     pub user: Signer<'info>,
